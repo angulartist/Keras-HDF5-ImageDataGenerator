76e183dffb7831f2d2f41c1d07978b63ac916e92
angulartist <belabesmohammed@gmail.com>
2020-03-22 17:12:12 +0100

    writer contextmanager protocole

--- playground/make.py
+++ playground/make.py
@@ -1,6 +1,10 @@
-from keras.datasets import mnist
+import os
 
+from keras.datasets import mnist
+from albumentations import Compose, HorizontalFlip, RandomGamma, ToFloat, Resize
 import h5py as h5
+from concurrent.futures import ProcessPoolExecutor as PoolExecutor
+from pytictoc import TicToc
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -8,46 +12,86 @@ X_train = X_train.reshape(-1, 28, 28, 1)
 X_test = X_test.reshape(-1, 28, 28, 1)
 
 
-class HDF5ImageWriter(object):
-    def __init__(self, src, dims, X_key="images", y_key="labels", buffer_size=1024):
+class FileAlreadyOpenError(RuntimeError):
+    pass
 
-        self.db = h5.File(src, "x")
-        self.images = self.db.create_dataset(X_key, dims, dtype="float32")
-        self.labels = self.db.create_dataset(y_key, (dims[0],), dtype="uint8")
+
+class HDF5ImageWriter(object):
+    def __init__(self, src, dims, X_key="images", y_key="labels", buffer_size=512):
+
+        self.src: str = src
+        self.dims = dims
+        self.X_key: str = X_key
+        self.y_key: str = y_key
+        self.db = None
+        self.images = None
+        self.labels = None
         self.buffer_size = buffer_size
         self.buffer = {"tmp_images": [], "tmp_labels": []}
-        self.index = 0
+        self._index = 0
+
+    def __enter__(self):
+        if self.db is not None:
+            raise FileAlreadyOpenError("The HDF5 file is already open!")
+
+        self.db = h5.File(self.src, "w")
+        self.images = self.db.create_dataset(self.X_key, self.dims, dtype="float32")
+        self.labels = self.db.create_dataset(self.y_key, (self.dims[0],), dtype="uint8")
+
+        return self
+
+    def __exit__(self, type_, value, traceback):
+        self.__close()
 
     def add(self, images, labels):
         self.buffer["tmp_images"].extend(images)
         self.buffer["tmp_labels"].extend(labels)
 
         if len(self.buffer["tmp_images"]) >= self.buffer_size:
-            self.flush()
+            self.__flush()
 
-    def flush(self):
-        index = self.index + len(self.buffer["tmp_images"])
-        self.images[self.index : index] = self.buffer["tmp_images"]
-        self.labels[self.index : index] = self.buffer["tmp_labels"]
-        self.index = index
+    def __flush(self):
+        index = self._index + len(self.buffer["tmp_images"])
+        self.images[self._index : index] = self.buffer["tmp_images"]
+        self.labels[self._index : index] = self.buffer["tmp_labels"]
+        self._index = index
 
         self.buffer = {"tmp_images": [], "tmp_labels": []}
 
-    def close(self):
+    def __close(self):
         if len(self.buffer["tmp_images"]) > 0:
-            self.flush()
+            self.__flush()
 
         self.db.close()
 
 
-writer = HDF5ImageWriter(
-    src="/storage/datasets/mnist_test.h5", dims=(len(X_test), 28, 28, 1)
+pipeline = Compose(
+    [
+        HorizontalFlip(p=0.5),
+        RandomGamma(gamma_limit=(80, 120), p=0.5),
+        HorizontalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+        HorizontalFlip(p=0.5),
+    ]
 )
 
-for (i, (x, y)) in enumerate(zip(X_test, y_test)):
-    writer.add([x], [y])
 
-    if i % 1024 == 0:
-        print("Images written", i)
+def transform(sample):
+    x, y = sample
 
-writer.close()
+    return pipeline(image=x)["image"], y
+
+
+h5_writer = HDF5ImageWriter(
+    src="/storage/datasets/mnist_test.h5", dims=(len(X_train), 28, 28, 1)
+)
+
+with TicToc():
+    with h5_writer as writer:
+        with PoolExecutor(max_workers=os.cpu_count()) as executor:
+            for image, label in executor.map(transform, zip(X_train, y_train)):
+                print("Adding:", label)
+                writer.add([image], [label])
